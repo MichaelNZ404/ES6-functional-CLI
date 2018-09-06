586048f1a6375f4cb6f4d80d8afd7a6931732493
Michael Coleman <michael.coleman404@gmail.com>
2018-09-06 20:25:00 +1200

    basic placement logic for bus

--- run.js
+++ run.js
@@ -1,15 +1,24 @@
-var stdin = process.openStdin();
-
+// Init stdin listener
+const stdin = process.openStdin();
 stdin.addListener("data", function(input) {
-    let userInput = input.toString().trim();
-    let splitInput = userInput.split(' ');
-    processInput(splitInput);
+    processInput(input.toString().trim().split(' '));
 });
+
+let bus = null
 
 const processInput = (input) => {
     switch(input[0].toUpperCase()) { 
         case 'PLACE': { 
-            break; 
+            const [x, y, direction] = input[1].split(',');
+            const xValid = Number.isInteger(Number(x)) && 0 <= Number(x) && Number(x) <= 5;
+            const yValid = Number.isInteger(Number(y)) && 0 <= Number(y) && Number(x) <= 5;
+            const dirValid = ['NORTH', 'SOUTH', 'EAST', 'WEST'].includes(direction);
+            if(xValid && yValid && dirValid){
+                console.log("Placement successful: " + input);
+            }
+            else{
+                console.log("Placement invalid: " + input);
+            }
         } 
         case 'MOVE': { 
             break; 
