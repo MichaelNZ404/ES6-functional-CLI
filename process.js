7567033af61cb3c84e47abba1847a939b8f60cf6
Michael Coleman <michael.coleman404@gmail.com>
2018-09-06 22:27:51 +1200

    Refactored try catch from within case statements to wrap entire switch

--- process.js
+++ process.js
@@ -3,23 +3,21 @@ const closeSession = () => {
 }
 
 const processInput = (bus, input) => {
-    switch(input[0].toUpperCase()) { 
-        case 'PLACE': { 
-            const [x, y, direction] = input[1].split(',');
-            const xValid = Number.isInteger(Number(x)) && 0 <= Number(x) && Number(x) <= 5;
-            const yValid = Number.isInteger(Number(y)) && 0 <= Number(y) && Number(x) <= 5;
-            const dirValid = ['NORTH', 'SOUTH', 'EAST', 'WEST'].includes(direction);
-            if(xValid && yValid && dirValid){
-                bus = [Number(x), Number(y), direction]
+    try{
+        switch(input[0].toUpperCase()) {
+            case 'PLACE': { 
+                const [x, y, direction] = input[1].split(',');
+                const xValid = Number.isInteger(Number(x)) && 0 <= Number(x) && Number(x) <= 5;
+                const yValid = Number.isInteger(Number(y)) && 0 <= Number(y) && Number(x) <= 5;
+                const dirValid = ['NORTH', 'SOUTH', 'EAST', 'WEST'].includes(direction);
+                if(xValid && yValid && dirValid){
+                    return [Number(x), Number(y), direction]
+                }
+                else{
+                    throw "Placement invalid: " + input;
+                }
             }
-            else{
-                console.log("Placement invalid: " + input);
-            }
-            return bus;
-        } 
-
-        case 'MOVE': {
-            try{
+            case 'MOVE': {
                 if(bus === null){
                     throw "Bus not initialized";
                 }
@@ -54,14 +52,8 @@ const processInput = (bus, input) => {
                     }
                 }
             }
-            catch(error){
-                console.error(error)
-                return bus;
-            }
-        }
 
-        case 'LEFT': {
-            try{
+            case 'LEFT': {
                 if(bus === null){
                     throw "Bus not initialized";
                 }
@@ -84,14 +76,8 @@ const processInput = (bus, input) => {
                     }
                 }
             }
-            catch(error){
-                console.error(error)
-                return bus;
-            }
-        }
 
-        case 'RIGHT': {
-            try{
+            case 'RIGHT': {
                 if(bus === null){
                     throw "Bus not initialized";
                 }
@@ -114,29 +100,22 @@ const processInput = (bus, input) => {
                     }
                 }
             }
-            catch(error){
-                console.error(error)
-                return bus;
-            }
-        }
 
-        case 'REPORT': {
-            try{
+            case 'REPORT': {
                 if(bus === null){
                     throw "Bus not initialized";
                 }
+                console.log("Bus is in position: " + bus);
+                return bus;
             } 
-            catch(error){
-                console.error(error)
-                return bus;
-            }
-            console.log("Bus is in position: " + bus);
-            return bus;
-        } 
-        default: { 
-            console.error("Invalid command: " + input);
-            return bus;
-        } 
+            default: { 
+                throw "Invalid command: " + input;
+            } 
+        }
+    }
+    catch(error){
+        console.error(error)
+        return bus;
     }
 }
 
